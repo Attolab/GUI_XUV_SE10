e597d89f94551d0b1827f6f98eed1ba758565a28
Thomas Murat <thomas.g.murat@gmail.com>
2022-07-19 16:39:50 +0200

    new csv updated

--- Readtxt.py
+++ Readtxt.py
@@ -2,8 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 
-# specter = np.zeros(10001)
-# tof = [2.000000026702864e-10*i for i in range(10001)]
+specter = np.zeros(10001)
+tof = [2.000000026702864e-10*i for i in range(10001)]
+
+# with open("Ne- calibration 30 V retarding potential_0.txt") as f:
+#     lines = f.readlines()
+#     lines = lines[0].split("\t")
+#     lines[-1] = lines[-1].split("\n")[0]
+#     lines = [float(l) for l in lines]
+#     lines = np.array(lines)
+
 
 # for i in range(19):
 #     with open("Zr"+str(i)+".txt") as f:
@@ -22,17 +30,20 @@ import csv
 #         for j in range(len(specter)):
 #             specter[j]+=lines[j]/19
 
-# np.save("specter",arr=specter)
+# np.save("Ne30",arr=lines)
 # a = np.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
 # np.savetxt("foo.csv", a, delimiter=",")
 
-with open("Ne-CH3i_30.csv") as file_name:
+with open("Ne-CH3i_20.csv") as file_name:
     file_read = csv.reader(file_name)
     array = list(file_read)
     array = array[1:]
     x = [float(array[i][0]) for i in range(len(array))]
     y = [float(array[i][1]) for i in range(len(array))]
-    
+
 
 plt.plot(x,y)
+
+plt.xlabel("Energie (eV)")
+plt.ylabel("Signal (mV)")
 plt.show()
