e3d304536f982f9011b372484f17f55a7fdf5f77
Thomas Murat <thomas.g.murat@gmail.com>
2022-07-27 17:02:08 +0200

    commented readtxt

--- Readtxt.py
+++ Readtxt.py
@@ -6,6 +6,8 @@ from scipy.stats import norm
 specter = np.zeros(10001)
 tof = [2.000000026702864e-10*i for i in range(10001)]
 
+#This is used for reading .txt files and combining specters with Al and Zr filters, check CXRO
+
 # with open("Ne- calibration 30 V retarding potential_0.txt") as f:
 #     lines = f.readlines()
 #     lines = lines[0].split("\t")
@@ -31,10 +33,15 @@ tof = [2.000000026702864e-10*i for i in range(10001)]
 #         for j in range(len(specter)):
 #             specter[j]+=lines[j]/19
 
+#This is used to save an array into an .npy file to be read later
+
 # np.save("Ne30",arr=lines)
 # a = np.asarray([ [1,2,3], [4,5,6], [7,8,9] ])
 # np.savetxt("foo.csv", a, delimiter=",")
 
+#This is used to read a csv file and plot it with gaussians for comparison
+
+#You have to put the csv in the same folder as of now
 with open("Ne-CH3i_20_Xe_calib.csv") as file_name:
     file_read = csv.reader(file_name)
     array = list(file_read)
@@ -42,23 +49,28 @@ with open("Ne-CH3i_20_Xe_calib.csv") as file_name:
     x = [float(array[i][0]) for i in range(len(array))]
     y = [float(array[i][1]) for i in range(len(array))]
 
+omega = 1.54 #differnce between harmonic and sideband
 
 def gaussian(x,peak,sigma):
     g=0
     for i in range(5):
-        g += 0.2*norm.pdf(x, loc = peak+3.1*i, scale = sigma)
-        g += 0.4*norm.pdf(x, loc = peak+1.8+3.1*i, scale = sigma)
+        #always a 2 : 1 difference between binding energies peaks
+        g += 0.2*norm.pdf(x, loc = peak+2*omega*i, scale = sigma)
+        g += 0.4*norm.pdf(x, loc = peak+1.8+2*omega*i, scale = sigma)
+
+        #for sidebands
+
+        # g -= 0.01*norm.pdf(x, loc = peak+omega*(2*i+1), scale = sigma)
+        # g -= 0.02*norm.pdf(x, loc = peak+1.8+omega*(2*i+1), scale = sigma)
     return g
 
 
-
-
-peak = 55*1.54-58.4
-sigma = 0.3
+peak = 57*omega-58.4 #first harmonic to plot
+sigma = 0.3 #width
 y_fit = np.zeros(len(y))
 
 for j in range(len(y_fit)):
-    y_fit[j] = gaussian(x[j],peak,sigma)
+     y_fit[j] = gaussian(x[j],peak,sigma)
 
 
 plt.figure()
@@ -69,7 +81,7 @@ plt.plot(x,y_fit)
 plt.xlabel("Energie (eV)")
 plt.ylabel("Signal (mV)")
 
-plt.xlim(20, 50)
-plt.ylim(-1,2)
+plt.xlim(25, 50)
+# plt.ylim(-0.1,0.1)
 
 plt.show()
